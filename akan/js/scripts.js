e556c25be831ed6af7caf3ba66aac889f5e501d9
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-09 17:02:06 +0300

    Getting data from the two input

--- akan/js/scripts.js
+++ akan/js/scripts.js
@@ -1,7 +1,8 @@
 
 //get the day of birth 
 function claculateBirthDay() {
-    
+    var gender=document.getElementById('gender').value;
+    var birthday=document.getElementById('birthday').value;
     
 }
 
