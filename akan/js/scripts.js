9307df8bc360ac3bd6dcd3a496d8160d42974563
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-10 08:49:08 +0300

    getting last year digit

--- akan/js/scripts.js
+++ akan/js/scripts.js
@@ -5,12 +5,18 @@ function claculateBirthDay() {
     var gender=document.getElementById('gender').value;
     var birthday=document.getElementById('birthday').value;
     //splitting the date to get the date,month,year in an array
-    var birthDateSplit = birthday.split("-");    
+    var birthDateSplit = birthday.split("-"); 
+    //getting day    
     var DD=parseInt(birthDateSplit.pop());
+    //getting month
     var MM=parseInt(birthDateSplit.pop());
+    //getting full year
     var YY=birthDateSplit.pop();
+    //getting century from year
     var CC=parseInt(YY.slice(0,2))
-    console.log(CC)
+    //getting the last year digit
+    YY=parseInt(YY.slice(2,4))
+    console.log(YY)
 }
 
 //
