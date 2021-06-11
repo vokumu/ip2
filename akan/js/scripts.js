eeedd600e356312260c501e98aa0d4f5aa2d413b
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-11 05:48:57 +0300

    adding getAkanName to js

--- akan/js/scripts.js
+++ akan/js/scripts.js
@@ -18,3 +18,58 @@ function getDayofWeek(birthday){
     day_of_the_week=day_of_the_week.toFixed(0)
     return day_of_the_week
 }
+
+function getAkanName(day,gender){
+    maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+    femaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+    if(gender=="male"){
+        if(day==0){
+            akanName=maleNames[0]
+        }
+        else if(day==1){
+            akanName=maleNames[1]
+        }
+        else if(day==2){
+            akanName=maleNames[2]
+        }
+        else if(day==3){
+            akanName=maleNames[3]
+        }
+        else if(day==4){
+            akanName=maleNames[4]
+        }
+        else if(day==5){
+            akanName=maleNames[5]
+        }
+        else if(day==6){
+            akanName=maleNames[6]
+        }
+    }
+    else if(gender==='female' ){
+        if(day==0){
+            akanName=femaleNames[0]
+        }
+        else if(day==1){
+            akanName=femaleNames[1]
+        }
+        else if(day==2){
+            akanName=femaleNames[2]
+        }
+        else if(day==3){
+            akanName=femaleNames[3]
+        }
+        else if(day==4){
+            akanName=femaleNames[4]
+        }
+        else if(day==5){
+            akanName=femaleNames[5]
+        }
+        else if(day==6){
+            akanName=femaleNames[6]
+        }
+    }
+    else{
+
+    }
+    return akanName;
+}
